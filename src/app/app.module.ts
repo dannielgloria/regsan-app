1ef3c7c6b71ab1871cf09660380ad7a083506009
arano1212 <arano1212@gmail.com>
2024-08-07 20:26:33 -0600

    feat: start of the navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SidebarModule } from './components/sidebar.module';
 
 @NgModule({
   declarations: [
@@ -10,7 +9,7 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    SidebarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
