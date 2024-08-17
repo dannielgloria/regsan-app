b3feebfde7e93cb34c3d1159b00b1eabf40e4b28
arano1212 <arano1212@gmail.com>
2024-08-16 20:22:04 -0600

    feat: first look login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', redirectTo: '/login', pathMatch: 'full' },
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
   { path: 'login', loadChildren: () => import('./login/login.module').then(m => m.LoginModule) },
   { path: 'admin', loadChildren: () => import('./admin/admin.module').then(m => m.AdminModule) },
   { path: 'sales', loadChildren: () => import('./sales/sales.module').then(m => m.SalesModule) },
