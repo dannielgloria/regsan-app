2a51cfdac6dc08a8cb9faee6cf72ce835b41a24e
cris-zs <cristinaz0597@gmail.com>
2024-08-16 23:22:36 -0600

    Base para formulario de recuperación de pwd

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,41 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { MustMatch } from './_helpers/must-match.validator';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  pwdForm!: FormGroup;
+    submitted = false;
+    constructor(private formBuilder: FormBuilder) { }
+    ngOnInit() {
+      this.pwdForm = this.formBuilder.group({
+          email: ['', [Validators.required, Validators.email]],
+          confirmEmail: ['', Validators.required]
+      }, {
+          validators: MustMatch('email', 'confirmEmail')
+      });
+  }
+
+  // convenience getter for easy access to form fields
+  get f() { return this.pwdForm.controls; }
+
+  onSubmit() {
+    this.submitted = true;
+
+    // stop here if form is invalid
+    if (this.pwdForm.invalid) {
+        return;
+    }
+
+    // display form values on success
+    alert('SUCCESS!! :-)\n\n' + JSON.stringify(this.pwdForm.value, null, 4));
+}
+onReset() {
+  this.submitted = false;
+  this.pwdForm.reset();
+}
   title = 'regsan-app';
 }
