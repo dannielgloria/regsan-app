afbcc177bc7977e4e904d7586754c2c7c1abd1f1
Grizzielicious <isloal9@gmail.com>
2024-08-19 01:36:36 -0600

    feat: se agrega el navbar imitando las capturas de pantalla proporcionadas rutas e información complementaria pendientes

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,11 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
+import { NavbarComponent } from './app/navbar/navbar.component';
 
 
 platformBrowserDynamic().bootstrapModule(AppModule)
+  .catch(err => console.error(err));
+
+  platformBrowserDynamic().bootstrapModule(NavbarComponent)
   .catch(err => console.error(err));
